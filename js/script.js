b5d939edf9519d479babff0c13d6d8e8abbcd72a
Andrés Marín <amarinpublico@gmail.com>
2023-02-25 15:58:05 +0100

    Cards section

--- js/script.js
+++ js/script.js
@@ -1,45 +1,45 @@
 'use strict'
 
-// Apps Slider
+// cards Slider
 
-var apps_container = document.querySelector('.apps-container'),
-    apps_slider = document.querySelector('.apps-slider'),
-    apps_section = document.querySelector('.apps-section');
+var cards_container = document.querySelector('.cards-container'),
+    cards_slider = document.querySelector('.cards-slider'),
+    cards_section = document.querySelector('.cards-section');
 
 var position = 0,
-    limit = (apps_container.childElementCount) - 1,
+    limit = (cards_container.childElementCount) - 1,
     x,
     x_prev,
     touched;
 
-apps_container.addEventListener('touchstart', (e) => {
+cards_container.addEventListener('touchstart', (e) => {
     x_prev = e.touches[0].clientX;
     touched = true;
 });
 
-apps_container.addEventListener('touchmove', (e) => {
+cards_container.addEventListener('touchmove', (e) => {
     x = e.touches[0].clientX;
     if (touched){
         if (x < x_prev) {
-            // Derecha
+            // Right
             if (position !== limit){
                 position++;
-                let width_app = apps_container.children[position].getBoundingClientRect().x,
-                    gap_app = apps_container.getBoundingClientRect().x;
-                apps_container.scrollLeft += width_app - gap_app;
-                apps_slider.children[position-1].classList.remove('active');
-                apps_slider.children[position].classList.add('active');
+                let width_card = cards_container.children[position].getBoundingClientRect().x,
+                    gap_card = cards_container.getBoundingClientRect().x;
+                cards_container.scrollLeft += width_card - gap_card;
+                cards_slider.children[position-1].classList.remove('active');
+                cards_slider.children[position].classList.add('active');
                 console.log(position)
             }
         } else if (x > x_prev) {
-            // Izquierda
+            // Left
             if (position !== 0){
                 position--;
-                let width_app = apps_container.children[position].getBoundingClientRect().x,
-                    gap_app = apps_container.getBoundingClientRect().x;
-                apps_container.scrollLeft += width_app - gap_app;
-                apps_slider.children[position+1].classList.remove('active');
-                apps_slider.children[position].classList.add('active');
+                let width_card = cards_container.children[position].getBoundingClientRect().x,
+                    gap_card = cards_container.getBoundingClientRect().x;
+                cards_container.scrollLeft += width_card - gap_card;
+                cards_slider.children[position+1].classList.remove('active');
+                cards_slider.children[position].classList.add('active');
                 console.log(position)
             }
         }
